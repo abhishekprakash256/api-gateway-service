123b12c71c8d447a6684bcdb651c9c2df11f8c36
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-05-05 22:19:56 -0400

    tests and build passed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SectionController } from './section/section.controller';
 import { SearchController } from './search/search.controller';
-import { AuthModule } from './auth/auth.module';
-import { CmsApiModule } from './cms-api/cms-api.module'; // ✅ Import CmsApiModule
+
 
 @Module({   
-  imports: [CmsApiModule, AuthModule], // ✅ Import CmsApiModule
   controllers: [AppController, SectionController, SearchController],
   providers: [AppService],
 })
